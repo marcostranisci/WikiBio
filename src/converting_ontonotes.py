dd64ae8c0ff06dc0ef1be94b2207779fda2abbe6
marcostranisci <marcoantonio.stranisci@unito.it>
2022-09-22 13:26:47 +0200

    new stuff here

--- src/converting_ontonotes.py
+++ src/converting_ontonotes.py
@@ -13,13 +13,14 @@ def parse_sents(a_list):
         d = dict()
 
         try:
-            d['sentence'] = [w[3] for w in a_list[idx]]
-            d['token_ids'] = [w[2] for w in a_list[idx]]
-            d['lemma'] = [w[6] for w in a_list[idx]]
-            d['pos'] = [w[4] for w in a_list[idx]]
+            a_list[idx] = [w for w in a_list[idx] if len(w)>6]
+            d['sentence'] = [w[3] for w in a_list[idx] if len(w)>6]
+            d['token_ids'] = [w[2] for w in a_list[idx] if len(w)>6]
+            d['lemma'] = [w[6] for w in a_list[idx] if len(w)>6]
+            d['pos'] = [w[4] for w in a_list[idx] if len(w)>6]
             d['doc'] = a_list[0][0][0]
 
-            verbs = [(w[6],w.index(key),a_list[idx].index(w)) for w in a_list[idx] if key in w]
+            verbs = [(w[6],w.index(key),a_list[idx].index(w)) for w in a_list[idx] if key in w and len(w)>6]
             if len(verbs)>0:
                 for verb in verbs:
                     length = len(set([a_list[idx][i][verb[-2]] for i in range(len(a_list[idx])-1)]))
@@ -39,7 +40,7 @@ def parse_sents(a_list):
             else:
                 labels = ['0' for i in d['sentence']]
                 d['labels'] = labels
-        except Exception as e: print(e)
+        except Exception as e: print('{},{}'.format(e,a_list[idx][0]))
 
         def_d[idx] = d
         '''merged= list(zip(id_sent_position_token,labels))
@@ -66,23 +67,29 @@ def from_file_to_list(file):
     return x
 
 
-def multi_doc_conversion(a_path):
+def multi_doc_conversion(a_path,mod = False):
     conv_d = list()
+
     for doc in glob.glob(a_path):
 
         converted = from_file_to_list(doc)
 
         conv_d.append(converted)
+    if mod is True:
+        conv_d = modify_onto(conv_d)
 
     return conv_d
 
 def modify_onto(a_list):
-    light_v = []
+    a=0
+    b=0
+    light_v = ['be','become','make','seem','do','get','have']
+    names = [x[:-1] for x in open('../nombank/nombank.1.0.words').readlines()]
     for jsn in a_list:
         for sent in jsn:
             try:
                 for i,w in enumerate(jsn[sent]['lemma']):
-                    if w in light_w:
+                    if w in light_v:
                         lemmas = jsn[sent]['lemma'][i:i+4]
                         pos = jsn[sent]['pos'][i:i+4]
                         is_name = False
@@ -91,37 +98,13 @@ def modify_onto(a_list):
                                 jsn[sent]['labels'][i]=0
                                 jsn[sent]['labels'][i+lemmas.index(lemma)]='EVENT'
                                 is_name = True
-                        if is_name is False:
+                                a+=1
+                        '''if is_name is False:
                             for lemma in lemmas:
-                                if lemma in names and re.search('JJ',pos[lemmas.index(lemma)]):
+                                if re.search('JJ',pos[lemmas.index(lemma)]):
                                     jsn[sent]['labels'][i]=0
                                     jsn[sent]['labels'][i+lemmas.index(lemma)]='EVENT'
-            except Exception as e:log.info(f"Houston we've got a {e}")
-
-    return None
-
-'''
-texts = list()
-   ...: labels = list()
-   ...: idx = 0
-   ...: for el in x:
-   ...:     for b in el:
-   ...:         try:
-   ...:             for i,w in enumerate(el[b]['lemma']):
-   ...:                 if w in prova:
-   ...:                     lemmas = el[b]['lemma'][i:i+4]
-   ...:                     pos = el[b]['pos'][i:i+4]
-   ...:                     for lemma in lemmas:
-   ...:                         if lemma in names and re.search('NN',pos[lemmas.
-   ...: index(lemma)]):
-   ...:
-   ...:                             el[b]['labels'][i]=0
-   ...:                             el[b]['labels'][i+lemmas.index(lemma)]='EVEN
-   ...: T'
-   ...:                             #print(el[b]['labels'][i+lemmas.index(lemma)
-   ...: ])
-   ...:
-   ...:         except Exception as e:print(e)
-
-
-'''
+                                    b+=1'''
+            except Exception as e:print(f"Houston we've got a {e}")
+    print(a,b)
+    return a_list
