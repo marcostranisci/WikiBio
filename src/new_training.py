928e43854edc81166ee592745b00e9b95d28cc74
marcostranisci <marcoantonio.stranisci@unito.it>
2022-10-17 15:22:53 +0200

    added corpus and new experimental settings

--- src/new_training.py
+++ src/new_training.py
@@ -31,7 +31,7 @@ def collate_batch(batch):
     return btc
 
 
-onto_settings = ['orig','mod','mod_jj']
+'''onto_settings = ['orig','mod','mod_jj']
 for onto in onto_settings:
     texts,labels = preproc.MultiDoc4seq2seq.global_data('./timebank_tabular/',t_type='sents')
     model = Bert4EventExtraction(pretrained_model_name="distilbert-base-uncased",
@@ -61,36 +61,39 @@ for onto in onto_settings:
 
     tr = Trainer(model,training,eval,'{}_report{}_.csv'.format(onto,r))
 
+    tr.do_train()'''
+
+onto_settings = ['4_entities','4_entities_wide','4_entities_strict']
+for onto in onto_settings:
+    model = Bert4EventExtraction(pretrained_model_name="distilbert-base-uncased",
+        num_classes=2)
+
+    with open('./onto_def_{}.json'.format(onto)) as f:
+        jsn = json.load(f)
+
+    train_txt = [x['sentences'] for x in jsn]
+    train_labels = [x['labels'] for x in jsn]
+    shuffle(train_txt,random_state=r)
+    shuffle(train_labels,random_state=r)
+    train_txt = [x for y in train_txt for x in y]
+    train_labels = [x for y in train_labels for x in y]
+
+    with open('./onto_gum_preproc_sents.json') as f:
+        jsn = json.load(f)
+
+    texts = list()
+    labels = list()
+    for item in jsn:
+        for el in item:
+            for x in item[el]:
+                texts.append(x[0])
+                labels.append(x[1])
+
+    validate = data.vanilla_dataset("distilbert-base-uncased",texts,labels)
+    eval = DataLoader(validate,batch_size=10,shuffle=False)
+
+    train = data.vanilla_dataset("distilbert-base-uncased",train_txt[:len(validate)*3],train_labels[:len(validate)*3])
+    training = DataLoader(train,batch_size=10,shuffle=True)
+
+    tr = Trainer(model,training,eval,'{}_report_{}.csv'.format(onto,r))
     tr.do_train()
-
-model = Bert4EventExtraction(pretrained_model_name="distilbert-base-uncased",
-    num_classes=2)
-
-with open('./onto_def_4_entities.json') as f:
-    jsn = json.load(f)
-
-train_txt = [x['sentences'] for x in jsn]
-train_labels = [x['labels'] for x in jsn]
-shuffle(train_txt,random_state=r)
-shuffle(train_labels,random_state=r)
-train_txt = [x for y in train_txt for x in y]
-train_labels = [x for y in train_labels for x in y]
-train = data.vanilla_dataset("distilbert-base-uncased",train_txt,train_labels)
-
-training = DataLoader(train,batch_size=10,shuffle=True)
-
-with open('./onto_gum_preproc_sents.json') as f:
-    jsn = json.load(f)
-
-texts = list()
-labels = list()
-for item in jsn:
-    for el in item:
-        for x in item[el]:
-            texts.append(x[0])
-            labels.append(x[1])
-
-validate = data.vanilla_dataset("distilbert-base-uncased",texts,labels)
-eval = DataLoader(validate,batch_size=10,shuffle=False)
-tr = Trainer(model,training,eval,'{}_report{}_.csv'.format('entities',r))
-tr.do_train()
